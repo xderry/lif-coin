3014b0db89d40802576950699e70677fed5f59a9
Derry Shribman <xxderry@gmail.com>
2025-07-06 20:22:43 +0300

    update

--- lif-node/lif_node.js
+++ lif-node/lif_node.js
@@ -1,37 +1,22 @@
 #!/usr/bin/env node
-
 'use strict';
-
 process.title = 'lif_node';
-
-if (process.argv.indexOf('--help') !== -1
-    || process.argv.indexOf('-h') !== -1) {
-  console.error('See the bcoin docs at: https://github.com/bcoin-org/bcoin/tree/master/docs.');
-  process.exit(1);
-  throw new Error('Could not exit.');
-}
-
-if (process.argv.indexOf('--version') !== -1
-    || process.argv.indexOf('-v') !== -1) {
-  const pkg = require('../package.json');
-  console.log(pkg.version);
-  process.exit(0);
-  throw new Error('Could not exit.');
-}
-
 const FullNode = require('../lib/node/fullnode');
 
 const node = new FullNode({
-  file: true,
+  network: 'lif', // 'main'
+  file: false,
   argv: true,
   env: true,
   logFile: true,
   logConsole: true,
-  logLevel: 'debug',
+  logLevel: 'info',
   memory: false,
   workers: true,
   listen: true,
-  loader: require
+  //loader: require,
+  prefix: '~/lif.store',
+  coinbaseFlags: 'mined by lif-coin',
 });
 
 // Temporary hack
@@ -40,20 +25,25 @@ if (!node.config.bool('no-wallet') && !node.has('walletdb')) {
   node.use(plugin);
 }
 
-process.on('unhandledRejection', (err, promise) => {
+process.on('unhandledRejection', (err, promise)=>{
   throw err;
 });
-
-process.on('SIGINT', async () => {
+process.on('SIGINT', async()=>{
   await node.close();
 });
 
-(async () => {
+async function _main(){
   await node.ensure();
   await node.open();
   await node.connect();
   node.startSync();
-})().catch((err) => {
-  console.error(err.stack);
-  process.exit(1);
-});
+}
+async function main(){
+  try {
+    _main();
+  } catch(err){
+    console.error(err.stack);
+    process.exit(1);
+  }
+}
+main();
