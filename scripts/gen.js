b9854ae517c040a69504818fe929932e648c266d
Derry Shribman <xxderry@gmail.com>
2025-08-04 00:20:58 +0300

    update

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function assert(v, msg){ if (v) return; debugger; throw Error('assert: '+msg); }
-const Consensus = require('../lib/protocol/consensus');
+const assert = require('bsert');
+const consensus = require('../lib/protocol/consensus');
 const Networks = require('../lib/protocol/networks');
 const TX = require('../lib/primitives/tx');
 const Block = require('../lib/primitives/block');
@@ -12,32 +12,28 @@ function createGenesisBlock(options) {
   let flags = options.flags;
   let key = options.key;
   let reward = options.reward;
-
   if (!flags)
     flags = 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks';
   if (typeof flags=='string')
     flags = Buffer.from(flags, 'ascii');
-
   if (!key) {
     key = Buffer.from(''
       + '04678afdb0fe5548271967f1a67130b7105cd6a828e039'
       + '09a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c3'
       + '84df7ba0b8d578a4c702b6bf11d5f', 'hex');
   }
-
   if (!reward)
-    reward = 50 * Consensus.COIN;
-
+    reward = 50 * consensus.COIN;
   const tx = new TX({
     version: 1,
     inputs: [{
       prevout: {
-        hash: Consensus.ZERO_HASH,
+        hash: consensus.ZERO_HASH,
         index: 0xffffffff
       },
       script: new Script()
         .pushInt(0x1d00ffff) // ~4G hashing attempts needed
-        .pushPush(Buffer.from([4])) // on avarage even 1 nonce cycle (32^2).
+        .pushPush(Buffer.from([consensus.lif ? 2 : 4])) // on avarage even 1 nonce cycle (32^2).
         .pushData(flags)
         .compile(),
       sequence: 0xffffffff
@@ -48,92 +44,105 @@ function createGenesisBlock(options) {
     }],
     locktime: 0
   });
-
   const block = new Block({
     version: options.version,
-    prevBlock: Consensus.ZERO_HASH,
+    prevBlock: consensus.ZERO_HASH,
     merkleRoot: tx.hash(),
     time: options.time,
     bits: options.bits,
     nonce: options.nonce,
     height: 0
   });
-
   block.txs.push(tx);
-
   return block;
 }
 
 nets.main = createGenesisBlock({
   version: 1,
   time: 1231006505,
-  bits: 0x1d00ffff,
-  nonce: 2083236893
+  bits: 0x1d00ffff, nonce: 2083236893,
 });
 nets.lif = createGenesisBlock({
   version: 1,
   time: 1753572481,
-  bits: 0x207fffff,
-  nonce: 2083236893 // need to mine this block to get a value
+  //bits: 0x1d00ffff, nonce: ???, // 256*10sec = 1hour
+  //bits: 0x1e00ffff, nonce: ???, // 10sec
+  bits: 0x1f00ffff, nonce: 4297, // 0.1sec fast
 });
 
 nets.testnet = createGenesisBlock({
   version: 1,
   time: 1296688602,
-  bits: 0x1d010fff,
-  nonce: 0x1d00ffff,
+  bits: 0x1d00ffff, nonce: 414098458,
 });
 
 nets.regtest = createGenesisBlock({
   version: 1,
   time: 1296688602,
-  bits: 0x207fffff,
-  nonce: 2
+  bits: 0x207fffff, nonce: 2,
 });
 
 nets.simnet = createGenesisBlock({
   version: 1,
   time: 1401292357,
-  bits: 0x207fffff,
-  nonce: 2
+  bits: 0x207fffff, nonce: 2,
 });
 
+function str_diff(a, b){
+  let i;
+  for (i=0; i<a.length; i++){
+    if (a[i]!=b[i])
+      break;
+  }
+  if (i==a.length && i==b.length)
+    return -1;
+  console.log('pos '+i+' diff: '+a.slice(i, i+8)+' -> '+b.slice(i, i+8));
+  return i;
+}
 function diff_block(name, block, net_def){
   console.log('--------- '+name+' ---------------');
-  0 && console.log(block);
-  let hold = net_def.genesis.hash.reverse().toString('hex');
-  let hnew = block.rhash();
-  if (hold!=hnew)
-    console.log('diff new: ', hnew);
-  console.log('hash old: ', hnew);
-  let bold = net_def.genesisBlock;
-  let bnew = block.toRaw().toString('hex');
-  if (bold!=bnew)
-    console.log('diff new:', bnew);
-  console.log('diff old:', bold);
+  // complete block
+  let b_orig = net_def.genesisBlock;
+  let b_gen = block.toRaw().toString('hex');
+  if (b_orig!=b_gen){
+    console.log('DIFF block gen:', b_gen);
+    str_diff(b_orig, b_gen);
+  }
+  console.log('block orig:', b_orig);
+  // check orig header hash matchs computed
+  let h_orig = net_def.genesis.hash.toString('hex');
+  let h_orig_comp = new Block().fromHead(Buffer.from(b_orig, 'hex')).hash()
+    .toString('hex');
+  if (h_orig!=h_orig_comp)
+    console.log('DIFF hash orig comp:', h_orig_comp);
+  let h_gen = block.hash().toString('hex');
+  if (h_gen!=h_orig)
+    console.log('DIFF hash gen:', h_gen);
+  // check hash matches target
+  if (mine_range(block.toRaw().slice(0, 80), null, block.nonce, block.nonce)<0){
+    console.log('target not reached:', '0x'+block.bits.toString(16),
+      common.getTarget(block.bits));
+  }
+  console.log('hash orig:', h_orig);
 }
 
 const hash256 = require('bcrypto/lib/hash256');
-const sha256 = require('../lif-node/sha256');
-const sha256lif = require('../lif-node/sha256lif');
+const sha256 = require('bcrypto/lib/sha256');
+const _sha256 = require('../lib/utils/sha256');
+const sha256lif = require('../lib/utils/sha256lif');
+const hash256lif = require('../lib/utils/hash256lif');
 const mine = require('../lib/mining/mine');
 const common = require('../lib/mining/common');
-function rcmp(a, b) {
-  assert(a.length === b.length);
-  for (let i = a.length-1; i>=0; i--){
-    let cmp = a[i]-b[i];
-    if (cmp)
-      return cmp;
-  }
-  return 0;
-}
 function mine_single(header, target, nonce){
   let hash;
   header.writeUInt32LE(nonce, 76, true);
-  hash = sha256.digest(sha256.digest(header)); // 0.13M/sec
-  //hash = sha256lif.digest(sha256lif.digest(header)); // 0.13M/sec
-  //hash = hash256.digest(header); // 0.28M/sec
-  let found = rcmp(hash, target)<=0;
+  //hash = sha256.digest(sha256.digest(header)); // 0.22M/sec
+  //hash = _sha256.digest(_sha256.digest(header)); // 0.33M/sec
+  //hash = sha256lif.digest(_sha256.digest(header)); // 0.29M/sec
+  //hash = hash256lif.digest(header); // 0.29M/sec
+  //hash = hash256.digest(header); // 0.36M/sec
+  hash = consensus.pow_hash256.digest(header);
+  let found = mine.rcmp(hash, target)<=0;
   if (!found)
     return;
   console.log('found nonce', nonce, header.toString('hex'));
@@ -141,6 +150,10 @@ function mine_single(header, target, nonce){
 }
 
 function mine_range(header, target, min, max){
+  if (!target)
+    target = common.getTarget(header.readUInt32LE(72));
+  if (0)
+    return mine(header, target, min, max); // 0.28M/sec
   for (let nonce=min; nonce<=max; nonce++){
     if (mine_single(header, target, nonce))
       return nonce;
@@ -157,17 +170,17 @@ function do_mine(block){
   // sha256.digest(header); --> 0.25M/sec (6 times slower than NodeJS native)
   console.log('mining...');
   let header = block.toRaw().slice(0, 80);
-  let min = 2083236890; // nonce bitcoin genesis 2083236893
+  let min = 0; // nonce bitcoin genesis 2083236893
   let max = 0x100000000;
+  console.log(block.bits.toString(16));
   let target = common.getTarget(block.bits);
+  console.log(target.toString('hex'));
   let inc = 1000000;
   let nonce = -1;
   for (let i=min; i<=max; i+=inc){
     let start = Date.now();
     let _max = Math.min(max, i+inc-1);
-    //nonce = mine(header, target, i, _max); // 0.28M/sec
-    nonce = mine_range(header, target, i, _max+1);
-    console.log(nonce);
+    nonce = mine_range(header, target, i, _max);
     if (nonce>=0)
       break;
     let tm = Date.now()-start;
@@ -182,9 +195,14 @@ function do_mine(block){
 }
 
 diff_block('main', nets.main, Networks.main);
-1 && do_mine(nets.main);
-process.exit(0);
+consensus.set_net('lif');
 diff_block('lif', nets.lif, Networks.lif);
+consensus.set_net();
 diff_block('testnet', nets.testnet, Networks.testnet);
 diff_block('regtest', nets.regtest, Networks.regtest);
 diff_block('simnet', nets.simnet, Networks.simnet);
+0 && do_mine(nets.main);
+consensus.set_net('lif');
+1 && do_mine(nets.lif);
+consensus.set_net();
+
