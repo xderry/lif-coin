ea3e6682986782692933d70e66e52420f1077d65
Derry Shribman <xxderry@gmail.com>
2025-08-12 01:10:25 +0300

    update

--- scripts/lif_node.js
+++ scripts/lif_node.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 process.title = 'lif_node';
+const Network = require('../lib/protocol/network');
+Network.set('lif');
 const consensus = require('../lib/protocol/consensus');
 const FullNode = require('../lib/node/fullnode');
 const Miner = require('../lib/mining/miner');
+const Mnemonic = require('../lib/hd/mnemonic');
+const HDPrivateKey = require('../lib/hd/private');
+const KeyRing = require('../lib/primitives/keyring');
+const Address = require('../lib/primitives/address');
 
-consensus.set_type('lif');
 const node = new FullNode({
   network: 'lif', // 'main'
   file: false,
@@ -22,20 +27,48 @@ const node = new FullNode({
   coinbaseFlags: 'mined by lif-coin',
 });
 
+function bech32_address(mnemonicPhrase){
+  const mnemonic = Mnemonic.fromPhrase(mnemonicPhrase);
+  const hdPrivKey = HDPrivateKey.fromMnemonic(mnemonic);
+  const derivedKey = hdPrivKey.derive(44, true)
+  .derive(0, true).derive(0, true).derive(0).derive(0);
+  const keyRing = new KeyRing({privateKey: derivedKey.privateKey,
+    witness: true});
+  const net = Network.get();
+  // For LIF network, update bech32 prefix to 'lif'
+  console.log(net.addressPrefix); // bc? lif?
+  const address = keyRing.getKeyAddress('string', net);
+  return {
+    privateKey: derivedKey.privateKey.toString('hex'),
+    publicKey: keyRing.publicKey.toString('hex'),
+    address: address,
+    keyRing: keyRing
+  };
+}
+
 let mine = 1; //process.argv.includes('mine');
+let mine_priv = 'six clip senior spy fury aerobic volume sheriff critic number feature inside';
+let mine_address = null; //bech32_address(mine_priv).address;
+//console.log(`Mining address calculated: ${mine_address}`); 
+
 async function mineBlocks(n){
   const chain = node.chain;
   const miner = new Miner({chain});
   const entries = [];
+  //const miningAddress = new Address(mine_address);
+  const addresses = null; //[miningAddress];
+  console.log(`Mining ${n} blocks to address: ${mine_address}`);
   for (let i = 0; i < n; i++){
-    const job = await miner.cpu.createJob();
+    const job = await miner.cpu.createJob(null /*{addresses}*/);
     // Mine blocks all ten minutes apart from regtest genesis
     //job.attempt.time = chain.tip.time + (60 * 10); // fake time
     const block = await job.mineAsync();
-    0 && console.log('mined block:', block.format());
+    console.log(`Mined block ${i + 1}/${n}: ${block.hash().toString('hex')}`);
     const entry = await chain.add(block);
     entries.push(entry);
   }
+  
+  console.log(`Successfully mined ${n} blocks!`);
   return entries;
 }
 
