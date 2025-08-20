b113dfffb803e88423d5a2c12e5d602b70c3b3ba
Derry Shribman <xxderry@gmail.com>
2025-08-21 01:37:23 +0300

    update

---
+++ browser/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createRoot, render} from 'react-dom';
+import App from './app.tsx';
+// set favicon
+let link = document.createElement('link');
+link.rel = 'icon';
+link.href = 'lif-kernel/favicon.ico';
+document.head.appendChild(link);
+// add stylesheet
+link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'style.css';
+document.head.appendChild(link);
+// start app
+let _root = document.body.appendChild(document.createElement('div'));
+let root = createRoot(_root);
+root.render(<App />);
