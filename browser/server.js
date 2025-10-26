74f6c4c6f0e6d885cd6566a71eb6c7c7417af250
Derry Shribman <xxderry@gmail.com>
2025-10-26 22:56:13 +0200

    update

--- browser/server.js
+++ browser/server.js
@@ -5,17 +5,16 @@ const fs = require('bfile');
 const WSProxy = require('./wsproxy');
 
 const index = fs.readFileSync(`${__dirname}/index.html`);
-const app = fs.readFileSync(`${__dirname}/app.js`);
-const worker = fs.readFileSync(`${__dirname}/worker.js`);
 
 const proxy = new WSProxy({
-  ports: [8333, 18333, 18444, 28333, 28901]
+  // ports: [8333, 18333, 18444, 28333, 28901]
 });
 
-const server = bweb.server({
-  port: Number(process.argv[2]) || 8080,
-  sockets: false
-});
+let opt = {
+  port: Number(process.argv[2]) || 4000,
+  sockets: false,
+};
+const server = bweb.server(opt);
 
 server.use(server.router());
 
@@ -31,14 +30,22 @@ server.get('/', (req, res) => {
   res.send(200, index, 'html');
 });
 
+if (0){
+const app = fs.readFileSync(`${__dirname}/src/app.js`);
 server.get('/app.js', (req, res) => {
   res.send(200, app, 'js');
 });
+}
 
+if (0){
+const worker = fs.readFileSync(`${__dirname}/worker.js`);
 server.get('/worker.js', (req, res) => {
   res.send(200, worker, 'js');
 });
+}
 
 proxy.attach(server.http);
 
 server.open();
+
+console.log('server proxy started on port '+opt.port)
